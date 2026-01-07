7d6454d3eee55ee2ba68416d534d6369b835d865
Asad Mokarim <asadmokarim@yahoo.com>
2026-01-08 00:20:08 +0530

    Enable Flask app to run on specified port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, render_template, request, url_for, session
+import os
 import json
 
 
@@ -183,8 +184,11 @@ def results():
     )
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
 
